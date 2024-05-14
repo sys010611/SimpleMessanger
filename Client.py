ea1c297604cc098dc49977432e710be0ca260fcb
sys010611 <a97968765@gmail.com>
2024-05-14 16:22:38 +0900

    헤더, 내용 분리 / 프로토콜 수정 / 리팩토링

--- Client.py
+++ Client.py
@@ -1,52 +1,99 @@
+import random
 from socket import *
+from threading import *
 
 serverName = 'localhost'
 serverPort = 10000
-
 ID = ""
-ID = input("Enter Your ID : ")
 
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = ID
-clientSocket.sendto(message.encode(), (serverName, serverPort))
+def MakeSession(users):
+    sessionUserList = []
+    while True:  # 원하는 만큼 세션에 초청
+        oppositeID = input("세션에 초대할 상대방의 ID를 입력하시오 (초대 종료 시 '.' 입력) : ")
 
-userList, serverAddr = clientSocket.recvfrom(2048)
-userList = userList.decode()
-print("유저 목록 : ")
-print(userList)
+        if oppositeID == '.':
+            break
 
-users = userList.split('\n')
+        ip, port = "", ""
 
-sessionUserList = []
-while True: # 원하는 만큼 세션에 초청
-    oppositeID = input("세션에 초대할 상대방의 ID를 입력하시오 (초대 종료 시 '.' 입력) : ")
+        for user in users:
+            if oppositeID == user.split(',')[0]:
+                ip = user.split(',')[1]
+                port = user.split(',')[2]
 
-    if oppositeID == '.':
-        break
+        if ip == "" and port == "":
+            print("존재하지 않는 ID입니다.")
+        else:
+            sessionUserList.append(oppositeID + ',' + ip + ',' + port)
 
-    ip, port = "", ""
+    return sessionUserList
 
-    for user in users:
-        if oppositeID == user.split(',')[0]:
+def send():
+    while True:  # 세션의 사용자들에게 메시지 보내기
+        message = "MESSAGE "
+        message = message + ID + ' '
+        content = input()
+        message = message + '\n\n'
+        message = message + content
+        for user in sessionUserList:
             ip = user.split(',')[1]
             port = user.split(',')[2]
+            userAddr = (ip, int(port))
+            clientSocket.sendto(message.encode(), userAddr)
+def recv():
+    while True:
+        try:
+            message, userAddr = clientSocket.recvfrom(2048)
+            message = message.decode()
 
-    if ip == "" and port == "":
-        print("존재하지 않는 ID입니다.")
-    else:
-        sessionUserList.append(oppositeID + ',' + ip + ',' + port)
+            header = message.split('\n\n')[0]
+            if header.split(' ')[0] == "MESSAGE":
+                content = message.split('\n\n')[1]
+                sender = header.split(' ')[1]
 
-    print("현재 세션 유저 목록: ")
+                print(sender + ' : ' + content + '\n')
+            
+        except OSError as e:
+            pass
+
+
+if __name__ == '__main__':
+    clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+    ID = input("Enter Your ID : ")
+
+    #서버에 ID를 보냄
+    message = "JOIN "
+    message = message + ID + ' '
+    clientSocket.sendto(message.encode(), (serverName, serverPort))
+
+    #현재 온라인인 유저 목록 요청
+    message = "USERLIST "
+    clientSocket.sendto(message.encode(), (serverName, serverPort))
+
+    # 유저 리스트를 확실히 받고 넘어가기
+    while True:
+        try:
+            userList, serverAddr = clientSocket.recvfrom(2048)
+            break
+        except OSError as e:
+            pass
+
+    userList = userList.decode()
+    print("유저 목록 : ")
+    print(userList)
+
+    users = userList.split('\n')
+
+    sessionUserList = MakeSession(users)
+    print("만든 세션 유저 목록: ")
     for user in sessionUserList:
         print(user)
 
-while True: #세션의 사용자들에게 메시지 보내기
-    message = input("메시지 전송 : ")
-    for user in sessionUserList:
-        ip = user.split(',')[1]
-        port = user.split(',')[2]
+    print("내용을 입력하고 ENTER로 전송")
 
-        userAddr = (ip, int(port))
+    sender = Thread(target=send, args=())
+    receiver = Thread(target=recv, args=())
 
-        userSocket = socket(AF_INET, SOCK_DGRAM)
-        userSocket.sendto(message.encode(), userAddr)
+    sender.start()
+    receiver.start()
--- Server.py
+++ Server.py
@@ -6,26 +6,48 @@ serverPort = 10000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(("", serverPort))
 
-while True:
-    ID, clientAddr = serverSocket.recvfrom(2048)
-
+def AddUser():
+    print(message.decode())
+    ID = message.decode().split(' ')[1]
     content = []
     if path.exists("UserList.txt"):
         with open("UserList.txt", 'r') as f:
             users = f.read().split('\n')
-
             for user in users:
-                if user.split(',')[0] == ID.decode(): #동일 ID 유저 접속, 이전 정보는 제거
+                if user.split(',')[0] == ID:  # 동일 ID 유저 접속, 이전 정보는 제거
                     continue
                 else:
-                    content.append(user+'\n')
-
-    content.append(ID.decode() + ',' + str(clientAddr[0]) + ',' + str(clientAddr[1]))
+                    content.append(user + '\n')
+    content.append(ID + ',' + str(clientAddr[0]) + ',' + str(clientAddr[1]))  # 새로 접속한 유저 리스트에 추가
 
     with open("UserList.txt", 'w') as f:
         for line in content:
             f.write(line)
 
+def GiveOnlineUserList():
+    print(message.decode())
+
+    # # 현재 온라인인 유저만 골라내기
+    # userAddr = (user.split(',')[1], int(user.split(',')[2]))
+    # serverSocket.sendto("Online Check".encode(), userAddr)
+
+    # message = ""
+    # while True:
+    #     message, userAddr = serverSocket.recvfrom(2048)
+    #     if(message != ""):
+    #         break
+    #
+    # if(message == "ONLINE"):
+    #     content.append(user+'\n')
     with open("UserList.txt", 'r') as f:
         serverSocket.sendto(f.read().encode(), clientAddr)
 
+if __name__ == '__main__':
+    while True:
+        message, clientAddr = serverSocket.recvfrom(2048)
+
+        if message.decode().split(' ')[0] == "JOIN":
+            AddUser()
+
+        elif message.decode().split(' ')[0] == "USERLIST":
+            GiveOnlineUserList()
